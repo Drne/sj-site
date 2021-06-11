6ccffc6e1b25671396f9009b1877f7418329895e
Drew Colgin <drewcolgin@Drews-MacBook-Air.local>
2021-06-11 17:20:11 -0400

    new icons, css

--- src/components/BrandIcon.js
+++ src/components/BrandIcon.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function BrandIcon({img, link}) {
 
     return (
-        <div style={{width: 150, height: 150, padding: 10}}>
-            <img src={img} alt={''} style={{objectFit: 'cover', width: '100%', height: '100%'}}/>
+        <div style={{width: 150, height: 150, padding: 10}} className={'img-wrapper'}>
+            <img src={img} alt={''} style={{objectFit: 'cover', width: '100%', height: '100%'}} className={'hover-zoom'}/>
         </div>
     )
 }
